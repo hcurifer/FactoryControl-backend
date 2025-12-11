ab6853403c8261e266e4fef0f1f77565d1aa3bb2
hcurifer <hcurifer@myuax.com>
2025-12-12 00:34:01 +0100

    Conexion con base de datos
    Se realiza una conexion mediante URL con los datos en variables en un archivo oculto por GitHub, se prueba la conexion siendo OK, se integra requeriments

--- app/core/config.py
+++ app/core/config.py
@@ -5,9 +5,25 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FactoryControl API"
     API_V1_PREFIX: str = "/api/v1"
+    
+    # Variables del archivo .env
+    DB_USER: str = ""
+    DB_PASSWORD: str = ""
+    DB_HOST: str = ""
+    DB_PORT: int = 0
+    DB_NAME: str = ""
+
 
     class Config:
         env_file = ".env"
         extra = "allow"
+        
+    @property
+    def DATABASE_URL(self):
+        
+        return (
+            f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
 
 settings = Settings()
